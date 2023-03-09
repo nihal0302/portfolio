713f17b4909d062514a70518e6b19a2c8f159f2d
nihal0302 <125590239+nihal0302@users.noreply.github.com>
2023-03-09 13:42:39 +0530

    progress bar added

--- javascriptfile.js
+++ javascriptfile.js
@@ -18,28 +18,6 @@ const mobile_menu = document.querySelector('.mobile-nav')
 
 
 
-let calcScrollValue = () => {
-  let scrollProgress = document.getElementById("progress");
-  let progressValue = document.getElementById("progress-value");
-  let pos = document.documentElement.scrollTop;
-  let calcHeight =  document.documentElement.scrollHeight -
-  document.documentElement.clientHeight;
-  let scrollValue = Math.round((pos * 100) / calcHeight);
-  if (pos > 100) {
-    scrollProgress.style.display = "grid";
-  } else {
-    scrollProgress.style.display = "grid";
-  }
-  scrollProgress.addEventListener("click", () => {
-      window.scrollTo({
-        top: 23270, // Change this value to your desired position
-        behavior: 'smooth'
-      });
-  });
-  scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
-};
-
-window.onscroll = calcScrollValue;
 
 
 
